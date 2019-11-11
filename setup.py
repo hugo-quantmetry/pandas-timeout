9c7e5f0f6d9f5ba7fed85eb34f644885f365a247
Hugo Perrier <hperrier@quantmetry.com>
2019-11-11 18:47:42 +0100

    pytest

--- setup.py
+++ setup.py
@@ -20,14 +20,12 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Hugo Perrier",
     author_email='hperrier@quantmetry.com',
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
